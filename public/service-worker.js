4eac32b37470c39d1fb0048839c14b7adb1b1222
Alan Elam <aelam757@gmail.com>
2020-05-06 19:31:26 -0400

    finishing service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,6 +6,62 @@ const FILES_TO_CACHE = [
     "/db.js"
 ];
 
-const CACHE_NAME = "budget_cache";
-const DATA_CACHE_NAME = "data-cache";
+const CACHE_NAME = "budget_cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache =>{
+            console.log('Successful cache');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map( key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request);
+        });
+      })
+    );
+  });
