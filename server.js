f2892064b34d2bcaa94d803c249f7c504384eec6
Alan Elam <aelam757@gmail.com>
2020-05-20 20:04:27 -0400

    heroku

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static("public"));
 //     useFindAndModify: false,
 //   }
 // );
-mongoose.connect("mongodb:/aelam757@gmail.com:Sk8roswell!@ds119020.mlab.com:19020/heroku_w3nzs78w", {
+mongoose.connect("mongodb:/aelam757@gmail.com:Sk8roswell!@ds023912.mlab.com:23912/heroku_lwhmskdt", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
